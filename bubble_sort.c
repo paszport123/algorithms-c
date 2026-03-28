5009abf9da6ddc3922b20bf1e56a98fd592cd055
Pawel Paszko <paulo.paszko@gmail.com>
2026-03-28 13:08:27 +0100

    add bubble sort implementation

---
+++ bubble_sort.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void bubble_sort(int *p, int *q);
+int *create_array(int r);
+void print_array(int *tab, int r);
+void swap(int *wsk1, int *wsk2);
+
+
+int main(void)
+{
+	srand(time(NULL));
+	
+	int a; //rozmiar tablicy
+	
+		printf("podaj rozmiar tablicy: ");
+			scanf("%d",&a);
+	
+	int *t1=create_array(a);
+	
+	print_array(t1, a);
+	
+	bubble_sort(&t1[0], &t1[a-1]);
+	
+	print_array(t1, a);
+	
+	free(t1);
+	
+	return 0;
+}
+
+void bubble_sort(int *p, int *q)
+{
+	int *i; // * ?
+	int *j; // * ?
+	for(i=p ; i<q ; i++) // przesuwa po kolei do konca tablicy
+	{
+		for(j=p ; j<q-(i-p) ; j++) // koniec - (i - poczatek) //
+		{
+			if(*j>*(j+1)) // p+1 ? 
+			{
+				swap(j,j+1);
+			}
+		}
+	}
+	
+}
+
+int *create_array(int r)
+{
+	int *tab=malloc(r*sizeof(int));
+	
+	int i;
+	for(i=0 ; i<r ; i++)
+	{
+		tab[i]=rand() % (-100 - 100+1)+(-100) ;
+	}
+	
+	return tab;
+}
+
+void print_array(int *tab, int r)
+{
+	int i;
+	for(i=0 ; i<r ; i++)
+		printf(" %d |",tab[i]);
+	
+	printf("\n");
+}
+
+void swap(int *wsk1, int *wsk2)
+{
+	int zamiana;
+	zamiana=*wsk1;
+	*wsk1=*wsk2;
+	*wsk2=zamiana;
+}
+
